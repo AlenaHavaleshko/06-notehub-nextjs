8e39379a2af2588182ab46eea502a305f87200d1
Alena Havalehko <126282451+AlenaHavaleshko@users.noreply.github.com>
2025-08-03 10:05:17 +0300

    change folders name

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
-import TanstackProvider from "@/components/TanStackProvider/TanstackProvider";
+import TanStackProvider from "@/components/TanStackProvider/TanStackProvider";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -28,11 +28,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <TanstackProvider>
+        <TanStackProvider>
           < Header />
           {children}
           <Footer />
-        </TanstackProvider>
+        </TanStackProvider>
       </body>
     </html>
   );
