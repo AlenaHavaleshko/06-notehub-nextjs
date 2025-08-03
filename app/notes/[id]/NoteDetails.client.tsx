d5012a1db7a4fea1f46c0374497ed5a7320ee6c4
Alena Havalehko <126282451+AlenaHavaleshko@users.noreply.github.com>
2025-08-03 17:29:24 +0300

    last saves

--- app/notes/[id]/NoteDetails.client.tsx
+++ app/notes/[id]/NoteDetails.client.tsx
@@ -1,13 +1,14 @@
 "use client";
 
 import { useParams } from "next/navigation";
-import React from "react";
 import { useQuery } from "@tanstack/react-query";
 import { fetchNoteById } from "@/lib/api";
+import { useEffect, useState } from "react";
 import css from "./NoteDetailsClient.module.css";
 
 const NoteDetailsClient = () => {
   const { id } = useParams<{ id: string }>();
+  const [formattedDate, setFormattedDate] = useState<string | null>(null);
 
   const {
     data: note,
@@ -16,8 +17,16 @@ const NoteDetailsClient = () => {
   } = useQuery({
     queryKey: ["note", id],
     queryFn: () => fetchNoteById(id),
-    refetchOnMount: false, // щоб запит не дублювався ( тобто не завантажувати при монтуванны компонента ,тобто нотатка є, запиту на бекенд немає)
+    refetchOnMount: false,
   });
+
+  // Преобразуем дату только на клиенте
+  useEffect(() => {
+    if (note) {
+      const date = new Date(note.createdAt);
+      setFormattedDate(date.toLocaleString()); // Вы можете указать формат, который подходит для всех пользователей
+    }
+  }, [note]);
 
   if (isLoading) {
     return <p>Loading, please wait...</p>;
@@ -34,10 +43,55 @@ const NoteDetailsClient = () => {
           <h2>{note?.title}</h2>
         </div>
         <p className={css.content}>{note?.content}</p>
-        <p className={css.date}>Created: {new Date(note.createdAt).toLocaleString()}</p>
+        <p className={css.date}>Created: {formattedDate}</p>
       </div>
     </div>
   );
 };
 
 export default NoteDetailsClient;
+
+
+// "use client";
+
+// import { useParams } from "next/navigation";
+// import React from "react";
+// import { useQuery } from "@tanstack/react-query";
+// import { fetchNoteById } from "@/lib/api";
+// import css from "./NoteDetailsClient.module.css";
+
+// const NoteDetailsClient = () => {
+//   const { id } = useParams<{ id: string }>();
+
+//   const {
+//     data: note,
+//     isLoading,
+//     error,
+//   } = useQuery({
+//     queryKey: ["note", id],
+//     queryFn: () => fetchNoteById(id),
+//     refetchOnMount: false, // щоб запит не дублювався ( тобто не завантажувати при монтуванны компонента ,тобто нотатка є, запиту на бекенд немає)
+//   });
+
+//   if (isLoading) {
+//     return <p>Loading, please wait...</p>;
+//   }
+
+//   if (error || !note) {
+//     return <p>Something went wrong.</p>;
+//   }
+
+//   return (
+//     <div className={css.container}>
+//       <div className={css.item}>
+//         <div className={css.header}>
+//           <h2>{note?.title}</h2>
+//         </div>
+//         <p className={css.content}>{note?.content}</p>
+//         <p className={css.date}>Created: {new Date(note.createdAt).toLocaleString()}</p>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default NoteDetailsClient;
